f580aa5e19e35f8cfb12b42a084a74bdee728d65
brendamatias <brendamatias.sobral@gmail.com>
2020-05-14 22:47:17 -0300

    reset password

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const moment = require('moment');
 const crypto = require('crypto');
 
 const User = use('App/Models/User');
@@ -31,9 +32,37 @@ class ForgotPasswordController {
         }
       );
     } catch (err) {
-      return response
-        .status(err.status)
-        .send({ error: { message: 'Email não encontrado' } });
+      return response.status(err.status).send({
+        error: { message: 'E-mail não cadastrado.' },
+      });
+    }
+  }
+
+  async update({ request, response }) {
+    try {
+      const { token, password } = request.all();
+
+      const user = await User.findByOrFail('token', token);
+
+      const tokenExpired = moment()
+        .subtract('2', 'days')
+        .isAfter(user.token_created_at);
+
+      if (tokenExpired) {
+        return response.status(401).send({
+          error: { message: 'O token de recuperação está expirado.' },
+        });
+      }
+
+      user.token = null;
+      user.token_created_at = null;
+      user.password = password;
+
+      await user.save();
+    } catch (err) {
+      return response.status(err.status).send({
+        error: { message: 'Ocorreu um erro ao tentar alterar sua senha.' },
+      });
     }
   }
 }
